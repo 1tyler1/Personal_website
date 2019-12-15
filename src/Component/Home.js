f33eb2256f9c2183b1baaac257b757e193e2d0af
1tyler1 <mya12ga@yahoo.com>
2019-12-15 15:39:00 -0500

    pushing to heroku

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
     render() {
         return ( 
             <div className = "video-overlay">
-                <NavBar/ >
+                <NavBar/>
                 <video src = { Video } autoPlay = { true } loop = { true }> 
                 </video>  
                 <h1 className = "header-animation-1" > Tyler Lauren Designs </h1>     
--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default class Projects extends Component {
     render() {
-        return ( <
-            Link label = "example 1"
-            to = "https://morning-peak-19701.herokuapp.com" >
-            Project 1 <
-            /Link>
+        return (
+        <Link label = "example 1" to = "https://morning-peak-19701.herokuapp.com">
+            Project 1 
+        </Link>
         )
     }
 }
